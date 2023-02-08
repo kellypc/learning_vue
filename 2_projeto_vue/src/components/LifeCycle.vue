a487f525b621f7acd32d069f7ca549334b1a40f4
Kelly Cruz <kellycruz.ti@gmail.com>
2023-02-08 17:24:07 -0300

    LifeCycle - Hooks no vuejs

---
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Meu nome é: {{ name }}</h1>
+</template>
+
+<script>
+  export default {
+    name: 'LifeCycle',
+    data() {
+      return {
+        name: 'Ainda não sei'
+      }
+    },
+    created(){
+      setTimeout(() => {
+        this.name = "Kelly"
+      }, 1000)
+    },
+    mounted() {
+      setTimeout(() => {
+        this.name = "Desconhecido"
+      }, 2000)
+    }
+  }
+</script>
---
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Olá Vue!</h1>
+    <p>Meu nome é {{ name }} e trabalho como {{ jobTitle }}</p>
+  </div>
+  
+</template>
+
+<script>
+  export default {
+    name: 'PrimeiroComponente',
+    data() {
+      return {
+        name: "Kelly",
+        jobTitle: "Programadora"
+      }
+    }
+  }
+</script>
