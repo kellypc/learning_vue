bc857737dad22564acaf9fbf499126ced97f83ec
Kelly Cruz <kellycruz.ti@gmail.com>
2023-02-09 16:03:03 -0300

    Directives

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <p>Estou trabalhando no momento.</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novas oportunidades.</p>
     <p>Utilizo as seguintes tecnologias: </p>
     <ul>
       <li>Javascript</li>
       <li>Ruby on Rails</li>
       <li>Vuejs</li>
     </ul>
+    <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Info'
+    name: 'Info',
+    data() {
+      return {
+        esta_trabalhando: false,
+        mostrar_email: true,
+        email: "teste@gmail.com"
+      }
+    }
   }
 </script>
